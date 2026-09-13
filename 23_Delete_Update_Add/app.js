6db54edf22fa3cd793ef7f10002e619bbcec9949
Alfat393CSE <alfattasnimhasan@gmail.com>
2026-09-13 21:02:07 +0600

    make the public folder static so that any stylish part won't block by the public folder

--- 23_Delete_Update_Add/app.js
+++ 23_Delete_Update_Add/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const path = require("path");
 const rootDir = require("./utils/pathUtils");
 
 const app = express();
+app.use(express.urlencoded());
+app.use(express.static(path.join(rootDir, "public")));
 
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`server is running at http://localhost:${PORT}`);
 });
-
