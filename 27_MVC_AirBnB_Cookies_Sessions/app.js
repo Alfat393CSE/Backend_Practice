b72b98dda9d963f2cbae727e3662be9ca5b4f58b
Alfat393CSE <alfattasnimhasan@gmail.com>
2026-09-19 23:30:24 +0600

    first step od cookies set up implemented

--- 27_MVC_AirBnB_Cookies_Sessions/app.js
+++ 27_MVC_AirBnB_Cookies_Sessions/app.js
@@ -16,14 +16,21 @@ app.use(express.static(path.join(rootDir, "public")));
 app.set("view engine", "ejs");
 app.set("views", "views");
 
+app.use((req, res, next) => {
+  req.isLoggedIn = req.get("Cookie")?.split("=")[1] || false;
+  console.log(req.isLoggedIn);
+  next();
+});
+
 app.use(host);
 app.use("/user", store);
-app.use("user", (req, res, next) => {
+app.use("/user", (req, res, next) => {
   if (!req.isLoggedIn) {
     return res.redirect("/login");
   }
   next();
 });
+
 app.use(authRouter);
 app.use(pageNotFound);
 
--- 27_MVC_AirBnB_Cookies_Sessions/controller/authController.js
+++ 27_MVC_AirBnB_Cookies_Sessions/controller/authController.js
@@ -7,6 +7,6 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  req.isLoggedIn = true;
+  res.cookie("isLoggedIn", true);
   res.redirect("/");
 };
